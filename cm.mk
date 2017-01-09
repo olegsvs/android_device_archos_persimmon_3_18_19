331b7b2f37dbb215711dcc06f7a2060f03d7bc0e
olegsvs <oleg.texet@gmail.com>
2017-01-09 15:42:53 +0300

    add

---
+++ cm.mk
@@ -0,0 +1,24 @@
+## Specify phone tech before including full_phone
+#$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := 50 Cobalt
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/archos/persimmon/device_persimmon.mk)
+
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := persimmon
+PRODUCT_NAME := lineage_persimmon
+PRODUCT_BRAND := Archos
+PRODUCT_MODEL := Archos 50 Cobalt
+PRODUCT_MANUFACTURER := Archos
